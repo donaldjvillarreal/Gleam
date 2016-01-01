6c13b2a951f74faa1ba2ed969f3323756d98d268
MD Islam <mdislamwork@gmail.com>
2016-01-01 00:04:15 -0500

    Added initial models for tracks

--- gleam/urls.py
+++ gleam/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from tracks import urls as tracks_urls
 
 urlpatterns = [
-    url(r'^tracks/', tracks_urls),
+    url(r'^tracks/', include(tracks_urls)),
     url(r'^admin/', admin.site.urls),
 ]
--- tracks/admin.py
+++ tracks/admin.py
@@ -1,3 +1,9 @@
+# coding=utf-8
 from django.contrib import admin
 
-# Register your models here.
+from tracks import models
+
+admin.site.register(models.Track)
+admin.site.register(models.TrackItem)
+admin.site.register(models.TrackOrder)
+# admin.site.register(models.TrackInformation)
