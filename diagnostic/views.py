b96fd352a8c20c525de7b6e520deb1a938444e7e
MD Islam <mdislamwork@gmail.com>
2016-02-18 23:19:17 -0500

    Added HAMD pagination and fixed up alert and progress bar

--- diagnostic/views.py
+++ diagnostic/views.py
@@ -6,12 +6,34 @@ from collections import OrderedDict
 
 
 def hamd_survey(request):
+    if request.method == 'GET':
+        question_number = 1
+    else:
+        question_number = int(request.POST.get('current', 1))
+
+    if question_number > 21:
+        # calculate the total points here
+        total = 0
+        for value in request.session['bdi_dict'].values():
+            total += int(value)
+        return render(request, 'diagnostic/results.html', {'total': total})
+
+    if question_number > 1 and request.session['hamd_dict'] is None:
+        question_number = 1
+
+    if request.session.get('hamd_dict') is None:
+        request.session['hamd_dict'] = OrderedDict()
+
     if request.method == 'POST':
-        print request.POST
-    qa_list = []
-    for question in models.Question.objects.filter(survey__short_name='HAM-D').order_by('order'):
-        qa_list.append((question, models.Answer.objects.filter(question=question.pk).order_by('value')))
-    return render(request, 'diagnostic/hamd.html', {'qa_list': qa_list})
+        bdi_dict = request.session['hamd_dict']
+        bdi_dict[question_number - 1] = request.POST['answer']
+        request.session['hamd_dict'] = bdi_dict
+
+    question = models.Question.objects.filter(survey__short_name='HAM-D').get(order=question_number)
+    qa_set = (question, models.Answer.objects.filter(question=question.pk).order_by('value'))
+    return render(request, 'diagnostic/hamd-pagination.html', {'qa_set': qa_set,
+                                                               'current': question_number + 1,
+                                                               'progress': int((question_number / 21.0) * 100)})
 
 
 def bdi_survey_pagination(request):
@@ -39,6 +61,6 @@ def bdi_survey_pagination(request):
 
     question = models.Question.objects.filter(survey__short_name='BDI').get(order=question_number)
     qa_set = (question, models.Answer.objects.filter(question=question.pk).order_by('value'))
-    return render(request, 'diagnostic/hamd-pagination.html', {'qa_set': qa_set,
+    return render(request, 'diagnostic/bdi-pagination.html', {'qa_set': qa_set,
                                                                'current': question_number + 1,
                                                                'progress': int((question_number / 21.0) * 100)})
