3f505ede253f08fb08e2bbd38550127efd1d5123
MD Islam <mdislamwork@gmail.com>
2016-04-12 21:59:34 -0400

    Added client profile wireframe

--- therapist/urls.py
+++ therapist/urls.py
@@ -4,4 +4,5 @@ from therapist import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^client/profile', views.client_profile, name='client_profile'),
 ]
--- therapist/views.py
+++ therapist/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'therapist/feed.html', {'people': range(10)})
+
+
+def client_profile(request):
+    return render(request, 'therapist/client-profile.html', {})
