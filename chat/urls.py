dfe828090155c4c4740d42accba276375df5c77a
MD Islam <mdislamwork@gmail.com>
2016-04-24 19:05:47 -0400

    Escape HTML in messages

--- chat/urls.py
+++ chat/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from chat import views
 
 urlpatterns = [
-    url(r'^multichat/$', views.ChatView.as_view(), name='multichat_room'),
+    url(r'^$', views.ChatView.as_view(), name='multichat_room'),
 ]
