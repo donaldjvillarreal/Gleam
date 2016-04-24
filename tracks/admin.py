9b734c5063b92423ac028b48f028c59c5a050e5f
MD Islam <mdislamwork@gmail.com>
2016-04-24 18:52:57 -0400

    Removed tracks app

--- tracks/admin.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-from django.contrib import admin
-
-from tracks import models
-
-admin.site.register(models.Track)
-admin.site.register(models.TrackItem)
-admin.site.register(models.TrackOrder)
-# admin.site.register(models.TrackInformation)
