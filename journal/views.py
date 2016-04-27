bcc30fac0903db91d69874f7f9d3ccb2d46d68e2
donaldjvillarreal <donaldjvillarreal@localhost>
2016-04-27 16:17:30 -0400

    journal entry view has started.

--- journal/views.py
+++ journal/views.py
@@ -8,7 +8,6 @@ import requests
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from journal import forms, models
 
-
 # Create your views here.
 @login_required
 def entry(request):
@@ -103,21 +102,57 @@ def word_list(request):
 
 @login_required
 def list_view(request):
-    if request.method == 'GET':
+    user = User.objects.get(id=request.user.id)
 
+    entry_list = models.Entry.objects.filter(user=user).order_by('-created')
+    paginator = Paginator(entry_list, 5)
+
+    page = request.GET.get('page')
+    try:
+        entries = paginator.page(page)
+    except PageNotAnInteger:
+        entries = paginator.page(1)
+    except EmptyPage:
+        entries = paginator.page(paginator.num_pages)
+
+    if request.method == 'POST':
         user = User.objects.get(id=request.user.id)
-        entry_list = models.Entry.objects.filter(user=user).order_by('-created')
-        paginator = Paginator(entry_list, 5)
+        textSentiment = 'http://gateway-a.watsonplatform.net/calls/text/TextGetTextSentiment'
+        keywordExtraction = 'http://gateway-a.watsonplatform.net/calls/text/TextGetRankedKeywords'
+        key = 'fe9480c1e35a7ade844fa82699c8d7e6792f14bb'
+        entry_form = forms.EntryForm(data=request.POST)
 
-        page = request.GET.get('page')
-        try:
-            entries = paginator.page(page)
-        except PageNotAnInteger:
-            # If page is not an integer, deliver first page.
-            entries = paginator.page(1)
-        except EmptyPage:
-            # If page is out of range (e.g. 9999), deliver last page of results.
-            entries = paginator.page(paginator.num_pages)
+        if entry_form.is_valid():
+            journal_entry = entry_form.save(commit=False)
+            journal_entry.user = user
+            myText = request.POST.get('entry')
+            tdata = requests.get(textSentiment, {'apikey': key, 'text': myText, 'outputMode': 'json'})
+            tresponse = tdata.json()
+            journal_entry.sentiment_type = tresponse['docSentiment']['type']
+            if tresponse['docSentiment']['type'] == 'neutral':
+                journal_entry.sentiment_score = 0
+            else:
+                journal_entry.sentiment_score = tresponse['docSentiment']['score']
+            journal_entry.save()
 
+            # Keyword Extraction
+            kdata = requests.get(keywordExtraction,
+                                 {'apikey': key, 'text': myText, 'outputMode': 'json', 'sentiment': 1})
+            kresponse = kdata.json()
+
+            for keyword in kresponse['keywords']:
+                if keyword['sentiment']['type'] == 'neutral':
+                    keyScore = 0
+                else:
+                    keyScore = keyword['sentiment']['score']
+                models.Keywords.objects.create(entry=journal_entry,
+                                               text=keyword['text'],
+                                               relevance=keyword['relevance'],
+                                               sentiment_type=keyword['sentiment']['type'],
+                                               sentiment_score=keyScore)
+                return render(request, 'journal/listview.html', {'entry_form': entry_form, 'entries': entries,
+                                                              'paginator': paginator})
+        else:
+            print entry_form.errors
 
     return render(request, 'journal/listview.html', {'entries': entries, 'paginator': paginator})
