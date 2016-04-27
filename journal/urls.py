636999d04a234c73e7fa173d03de3f2ca46dc712
donaldjvillarreal <donaldjvillarreal@localhost>
2016-04-27 16:53:20 -0400

    journal changed.

--- journal/urls.py
+++ journal/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from journal import views
 
 urlpatterns = [
-    url(r'^$', views.list_view, name='listview'),
+    url(r'^$', views.entry, name='entry'),
+    url(r'^history/$', views.list_view, name='listview'),
     url(r'^entry/$', views.entry, name='entry'),
     url(r'^words/$', views.word_list, name='words'),
-    # url(r'^(?P<patient_id>[0-9]+)/therapist/$', views.TherapistTaskView.as_view(), name='create'),
 ]
