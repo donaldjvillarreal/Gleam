c633b2c7e3e2ce4f583c39cf7d4968f26493a386
MD Islam <mdislamwork@gmail.com>
2016-04-27 16:23:51 -0400

    Fixed URLS

--- core/urls.py
+++ core/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     # url(r'^$', views.index, name='index'),
     url(r'^progress/check/$', views.progress_delay, name='progress_check'),
     url(r'^therapist/$', views.therapist_home, name='therapist_home'),
-    url(r'', views.landing, name='landing'),
     url(r'^patient/$', views.patient_home, name='patient_home'),
+    url(r'^$', views.landing, name='landing'),
+
 ]
