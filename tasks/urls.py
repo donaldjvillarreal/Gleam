3fa070ddd2e55a90a4fb50fe5b9943c83106a350
MD Islam <mdislamwork@gmail.com>
2016-04-28 20:20:08 -0400

    Updated task url

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from tasks import views
 
 urlpatterns = [
-    url(r'^(?P<patient_id>[0-9]+)/therapist/$', views.TherapistTaskView.as_view(), name='create'),
+    url(r'^(?P<patient_id>[0-9]+)/edit/$', views.TherapistTaskView.as_view(), name='create'),
     url(r'^complete/$', views.PatientTaskView.as_view(), name='complete'),
 ]
